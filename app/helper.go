85b2a1eb70bac65c9cb6b9101a0b83a87b68885b
atharva <atharva.prabhune2021@gmail.com>
2025-06-14 15:08:46 +0530

    Added error logging & tracing

--- app/helper.go
+++ app/helper.go
@@ -2,23 +2,29 @@ package main
 
 import (
 	"fmt"
+	"runtime/debug"
 	"strconv"
-	
+
 	"unicode"
 )
 
-func decodeBencode(bencodedString string) (interface{}, error) {
+func (app *application) errorTrace(err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())	
+	app.errLog.Output(2, trace)
+}
+
+func (app *application) decodeBencode(bencodedString string) (interface{}, error) {
 	length := len(bencodedString)
 	if unicode.IsDigit(rune(bencodedString[0])) {
-		return decodeBencodeString(bencodedString, length)
+		return app.decodeBencodeString(bencodedString, length)
 	} else if rune(bencodedString[0]) == 'i' {
-		return decodeBencodeInt(bencodedString, length)
+		return app.decodeBencodeInt(bencodedString, length)
 	} else {
 		return "", fmt.Errorf("This format is not supported supported at the moment")
 	}
 }
 
-func decodeBencodeString(bencodedString string, n int) (interface{}, error) {
+func (app *application) decodeBencodeString(bencodedString string, n int) (interface{}, error) {
 	var firstColonIndex int
 	for i, character := range bencodedString {
 		if character == ':' {
@@ -29,15 +35,17 @@ func decodeBencodeString(bencodedString string, n int) (interface{}, error) {
 	lengthStr := bencodedString[:firstColonIndex]
 	length, err := strconv.Atoi(lengthStr)
 	if err != nil {
+		app.errorTrace(err)
 		return "", err
 	}
 	return bencodedString[firstColonIndex + 1 : firstColonIndex + 1 + length], nil
 }
 
-func decodeBencodeInt(bencodedString string, n int) (interface{}, error) {
+func (app *application) decodeBencodeInt(bencodedString string, n int) (interface{}, error) {
 	numStr :=bencodedString[1 : n - 1]
 	num, err := strconv.Atoi(numStr)
 	if err != nil {
+		app.errorTrace(err)
 		return "", err
 	}
 	return num, nil
--- app/main.go
+++ app/main.go
@@ -3,26 +3,27 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
-	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
 
-// Example:
-// - 5:hello -> hello
-// - 10:hello12345 -> hello12345
-
+type application struct {
+	errLog *log.Logger
+}
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	// fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
+
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Lshortfile)
+	app := &application {
+		errLog: errorLog,
+	}
 
 	command := os.Args[1]
 
 	if command == "decode" {
-		// Uncomment this block to pass the first stage
 		bencodedValue := os.Args[2]
 
-		decoded, err := decodeBencode(bencodedValue)
+		decoded, err := app.decodeBencode(bencodedValue)
 		if err != nil {
 			fmt.Println(err)
 			return
